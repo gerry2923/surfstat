9bc162e9aba97a7cf37a4c326ec1902607ce010a
GerryMON <mironova_plus@mail.ru>
2023-05-03 16:01:17 +0300

    main reg + footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
-import { addEventsMenu } from "./modules/main-menu.js";
+import { addEvents} from "./modules/main-menu.js";
+
 
 flsFunctions.isWebp();
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
 
-addEventsMenu();
+addEvents();
--- src/js/modules/main-menu.js
+++ src/js/modules/main-menu.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('.site-nav_show');
 const menu = document.querySelector('.main-header__menu');
 const loginMainHeader = document.querySelector('.main-header__login');
+const mainHeader = document.querySelector('.main-header');
 
 const showHideMenu = (evt)=> {
   if(menu.classList.contains('main-header__menu_hide')) {
@@ -8,18 +9,33 @@ const showHideMenu = (evt)=> {
     menu.classList.remove('main-header__menu_hide');
     loginMainHeader.classList.add('main-header__login_hide');
     evt.target.classList.add('site-nav_hide');
+    mainHeader.style.boxShadow = 'none';
 
   }
   else {
     menu.classList.add('main-header__menu_hide');
     loginMainHeader.classList.remove('main-header__login_hide');
     evt.target.classList.remove('site-nav_hide');
+    mainHeader.style.boxShadow = '0px 5px 13px 4px rgba(0, 0, 0, 0.09)';
   }
 }
 
 
-const addEventsMenu = () => {
+// ------------------------------------------------------------
+
+const searchBtn = document.querySelector('.t-header__input-btn');
+
+const showRegistrationPage0 = () => {
+  window.location = 'registration.html';
+
+};
+
+
+const addEvents = () => {
   menuBtn.addEventListener('pointerdown', showHideMenu);
+  searchBtn.addEventListener('pointerdown', showRegistrationPage0);
+
 }
 
-export {addEventsMenu};
+
+export {addEvents};

45f0f5b22414e52bd881be217188a8a0bc0486b9
GerryMON <mironova_plus@mail.ru>
2023-06-09 21:21:07 +0300

    time-interval

--- src/js/modules/time-interval-order.js
+++ src/js/modules/time-interval-order.js
@@ -3,9 +3,12 @@
 const timeIntervalSelect = document.querySelector('.time-interval');
 const timeIntervalBtn = document.querySelector('.time-interval__btn');
 
-//  тут меняем только белое
-const onChangeBtnColorWhite = (evt) => {
-  console.log('onChangeBtnColorWhite\n' + evt.target.classList[0]);
+//  Кнопка в активном состоянии имеет цвет синий, переключатель серый
+const onChangeBtnColorGray = (evt) => {
+  console.log('onChangeBtnColorGray\n' + evt.target.classList[0]);
+
+  evt.target.parentElement.classList.add('time-interval__container_active');
+
   if(timeIntervalBtn.dataset.order === 'desc') {
     timeIntervalBtn.classList.add('time-interval__btn_active-desc');
   } else {
@@ -16,7 +19,7 @@ const onChangeBtnColorWhite = (evt) => {
 
 const onChangeBtnColorBlue = (evt) => {
   console.log('onChangeBtnColorBlue\n' + evt.target.classList[0]);
- 
+ evt.target.parentElement.classList.remove('time-interval__container_active');
   if(timeIntervalBtn.dataset.order === 'desc') {
     timeIntervalBtn.classList.remove('time-interval__btn_active-desc');
   } else {
@@ -24,10 +27,9 @@ const onChangeBtnColorBlue = (evt) => {
   }
   
 };
-// тут меняем только синие
+// Кнопка в неактивном состоянии имеет цвет серый, а переключатель белый
 const onChangeBtnOrder = (evt) => {
-  console.log('changing order\n' + evt.target.classList[0]);
-  
+  console.log('changing order\n' + evt.target.classList);
   if(evt.target.dataset.order === 'asc') {
     timeIntervalBtn.dataset.order = 'desc';
     timeIntervalBtn.classList.add('time-interval__btn_desc');
@@ -42,7 +44,8 @@ const onChangeBtnOrder = (evt) => {
 
 export const initTimeIntervalSelection = () => {
   if(timeIntervalSelect) {
-    timeIntervalSelect.addEventListener('focusin', onChangeBtnColorWhite);
+    timeIntervalSelect.addEventListener('focusin', onChangeBtnColorGray);
+
     timeIntervalSelect.addEventListener('focusout', onChangeBtnColorBlue);
     timeIntervalBtn.addEventListener('pointerdown', onChangeBtnOrder);
   }
